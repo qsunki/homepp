a6bd2ffb8096d9e602519cd4a3a14f25bcf806e8
qsunki <ajsthfldu@outlook.com>
2024-07-15 10:56:26 +0900

    feat: 프로젝트 초기설정

---
+++ backend/src/main/java/ssafy/age/backend/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package ssafy.age.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable);  // CSRF 비활성화
+        return http.build();
+    }
+}
