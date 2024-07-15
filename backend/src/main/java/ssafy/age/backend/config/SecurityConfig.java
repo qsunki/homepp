a02837ba6ab31e06615c44fc3d1280b373dc50c2
qsunki <ajsthfldu@outlook.com>
2024-07-15 11:05:42 +0900

    feat: cors 비활성화

--- backend/src/main/java/ssafy/age/backend/config/SecurityConfig.java
+++ backend/src/main/java/ssafy/age/backend/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,8 +13,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(AbstractHttpConfigurer::disable)  // CORS 비활성화
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().permitAll()
+                )
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)  // sameOrigin으로 설정하여 동일 출처에서의 프레임 허용
                 )
                 .csrf(AbstractHttpConfigurer::disable);  // CSRF 비활성화
         return http.build();
