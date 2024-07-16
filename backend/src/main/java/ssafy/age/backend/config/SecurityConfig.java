d9676563a7fc772301d352a468e5262cce3d5831
qsunki <ajsthfldu@outlook.com>
2024-07-16 23:24:13 +0900

    feat: 로컬용 시큐리티 설정 분리

--- backend/src/main/java/ssafy/age/backend/config/SecurityConfig.java
+++ backend/src/main/java/ssafy/age/backend/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package ssafy.age.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
@@ -11,6 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
+    @Profile("local")
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .cors(AbstractHttpConfigurer::disable)  // CORS 비활성화
