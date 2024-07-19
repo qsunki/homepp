47f5ec5c4b997c9b1dd4f6cf2006bf6ceee0d2f0
유승현 <fview1020@gmail.com>
2024-07-19 11:41:30 +0900

    fix: security config ãcsrf 비활성화

--- backend/src/main/java/ssafy/age/backend/config/SecurityConfig.java
+++ backend/src/main/java/ssafy/age/backend/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.formLogin(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
