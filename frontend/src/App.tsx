088cf224b73c93303709e630ef63278b6df40240
Yoonha <yoonha0124@naver.com>
2024-07-22 13:28:57 +0900

    fix: git 이슈

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer/Footer';
 import LandingPage from './pages/landingpage/LandingPage';
 import HomePage from './pages/homepage/HomePage';
