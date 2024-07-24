d588830d05eb2a41a7a0e9535479dbc165c99e1d
이효림 <hlleee@naver.com>
2024-07-24 17:46:27 +0900

    feat: Video entity 작성

---
+++ backend/src/main/java/ssafy/age/backend/video/persistence/Video.java
@@ -0,0 +1,33 @@
+package ssafy.age.backend.video.persistence;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import ssafy.age.backend.event.persistence.Event;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Video {
+
+    @Id
+    @Column(name = "video_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "record_start_at")
+    private LocalDateTime recordStartAt;
+
+    @Column(name = "record_end_at")
+    private LocalDateTime recordEndAt;
+
+    private String url;
+
+    private Long length;
+
+    @OneToMany(mappedBy = "video")
+    private List<Event> eventList;
+
+}
