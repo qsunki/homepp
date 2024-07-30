1fc7fca86981bfc44d8fac94410e2992cdb48c67
qsunki <ajsthfldu@outlook.com>
2024-07-30 13:51:46 +0900

    fix: data.sql 테이블명 소문자로 변경

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -18,19 +18,19 @@ values (1, 'ROLE_USER'),
        (4, 'ROLE_USER'),
        (5, 'ROLE_USER');
 
-INSERT INTO `CAM` (`cam_id`, `name`, `ip`, `status`, `region`, `member_id`)
+INSERT INTO `cam` (`cam_id`, `name`, `ip`, `status`, `region`, `member_id`)
 VALUES (1, 'Cam1', '192.168.1.1', 'REGISTERED', 'Region1', 1),
        (2, 'Cam2', '192.168.1.2', 'REGISTERED', 'Region2', 2),
        (3, 'Cam3', '192.168.1.3', 'UNREGISTERED', 'Region3', 3),
        (4, 'Cam4', '192.168.1.4', 'UNREGISTERED', 'Region1', 4),
        (5, 'Cam5', '192.168.1.5', 'UNREGISTERED', 'Region2', 5);
 
-INSERT INTO `VIDEO` (`video_id`, `record_start_at`, `url`, `length`, `thumbnail_url`, `cam_id`)
+INSERT INTO `video` (`video_id`, `record_start_at`, `url`, `length`, `thumbnail_url`, `cam_id`)
 VALUES (1, '2023-07-29 10:00:00', 'http://example.com/video1.mp4', 120, 'http://example.com/thumb1.jpg', 1),
        (2, '2023-07-29 11:00:00', 'http://example.com/video2.mp4', 150, 'http://example.com/thumb2.jpg', 2),
        (3, '2023-07-29 13:00:00', 'http://example.com/video3.mp4', 180, 'http://example.com/thumb3.jpg', 1);
 
-INSERT INTO `EVENT` (`event_id`, `occurred_at`, `type`, `cam_id`, `video_id`)
+INSERT INTO `evnet` (`event_id`, `occurred_at`, `type`, `cam_id`, `video_id`)
 VALUES (1, '2023-07-29 10:00:00', 'INVASION', 1, 1),
        (2, '2023-07-29 11:00:00', 'SOUND', 2, 2),
        (3, '2023-07-29 12:00:00', 'FIRE', 1, 2),
@@ -39,7 +39,7 @@ VALUES (1, '2023-07-29 10:00:00', 'INVASION', 1, 1),
 
 
 
-INSERT INTO `ENV_INFO` (`env_info_id`, `recorded_at`, `temperature`, `humidity`, `status`, `cam_id`)
+INSERT INTO `env_info` (`env_info_id`, `recorded_at`, `temperature`, `humidity`, `status`, `cam_id`)
 VALUES (1, '2023-07-29 09:00:00', 25.5, 60.0, 'RECORDING', 1),
        (2, '2023-07-29 09:30:00', 26.0, 55.0, 'RECORDING', 2),
        (3, '2023-07-29 10:00:00', 24.0, 65.0, 'RECORDING', 1),
@@ -47,7 +47,7 @@ VALUES (1, '2023-07-29 09:00:00', 25.5, 60.0, 'RECORDING', 1),
        (5, '2023-07-29 11:00:00', 23.5, 50.0, 'OFFLINE', 1);
 
 -- 더미 데이터 삽입: SHARE 테이블
-INSERT INTO `SHARE` (`share_id`, `member_id`, `share_member_id`, `nickname`)
+INSERT INTO `share` (`share_id`, `member_id`, `share_member_id`, `nickname`)
 VALUES (1, 1, 2, '엄마'),
        (2, 1, 3, '아빠'),
        (3, 1, 4, '형'),
