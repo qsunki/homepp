6613480d71b7e81dd9a340324880b9932df4bf5c
Yoonha <yoonha0124@naver.com>
2024-08-01 10:57:55 +0900

    fix: 로그인 후 새로고침하면 로그인 풀리는 오류 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -7,13 +7,42 @@ import HomePage from './pages/HomePage';
 import MyPage from './pages/MyPage';
 import LiveVideo from './pages/LiveVideo';
 import VideoList from './pages/VideoList';
-import VideoDetail from 'pages/VideoDetail';
+import VideoDetail from './pages/VideoDetail';
 import ScrollToTop from './utils/ScrollToTop';
+import { useUserStore } from './stores/useUserStore';
+import { setAuthToken } from './api';
+import api from './api'; // api 인스턴스 가져오기
 import './App.css';
 import 'tw-elements';
 import 'tw-elements/dist/css/tw-elements.min.css';
 
 const App: React.FC = () => {
+  const { login, logout } = useUserStore();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setAuthToken(token);
+      // 토큰 유효성 확인
+      api
+        .get('/members')
+        .then((response) => {
+          const user = response.data;
+          if (user) {
+            login(user.userId, user.email, ''); // 사용자 정보를 설정합니다.
+          } else {
+            logout();
+          }
+        })
+        .catch((error) => {
+          console.error('토큰 유효성 검사 실패:', error);
+          logout();
+        });
+    } else {
+      logout();
+    }
+  }, [login, logout]);
+
   return (
     <Router>
       <ScrollToTop />
