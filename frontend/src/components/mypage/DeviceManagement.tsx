df83e2550e199d89ced1f321d8d54c67722e08ba
dongwook <cabbi120101@gmail.com>
2024-08-05 14:41:58 +0900

    feat : 공유 유저 추가 점검 과정12

--- frontend/src/components/mypage/DeviceManagement.tsx
+++ frontend/src/components/mypage/DeviceManagement.tsx
@@ -298,24 +298,3 @@ const DeviceManagement: React.FC = () => {
 };
 
 export default DeviceManagement;
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import Modal from 'react-modal';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error('Failed to find the root element');
-}
-
-Modal.setAppElement(rootElement);
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
