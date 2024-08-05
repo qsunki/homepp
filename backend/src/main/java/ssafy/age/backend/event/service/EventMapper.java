b25aae2f7a23192e176faff6a612ea49cf0980f4
qsunki <ajsthfldu@outlook.com>
2024-08-05 18:33:21 +0900

    feat: Event to EventDetailDto 매퍼 추가

--- backend/src/main/java/ssafy/age/backend/event/service/EventMapper.java
+++ backend/src/main/java/ssafy/age/backend/event/service/EventMapper.java
@@ -1,10 +1,12 @@
 package ssafy.age.backend.event.service;
 
+import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import ssafy.age.backend.event.persistence.Event;
 import ssafy.age.backend.event.web.EventResponseDto;
+import ssafy.age.backend.video.web.EventDetailDto;
 
 @Mapper
 public interface EventMapper {
@@ -15,4 +17,8 @@ public interface EventMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "isRead", ignore = true)
     Event toEvent(EventDto eventDto);
+
+    EventDetailDto eventToEventDetailDto(Event event);
+
+    List<EventDetailDto> map(List<Event> evnets);
 }
