1361a3946ab908452a4fb5bb5fe14f4c34ddcd26
qsunki <ajsthfldu@outlook.com>
2024-08-05 13:29:54 +0900

    fix: allowedMethods 다시 고침

--- backend/src/main/java/ssafy/age/backend/config/WebMvcConfig.java
+++ backend/src/main/java/ssafy/age/backend/config/WebMvcConfig.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("/*").allowedOrigins("*");
+        registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH").allowedOrigins("*");
     }
 }
