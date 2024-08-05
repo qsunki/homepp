4dd45dbe0895596efdc264425c663fb11c77fba0
qsunki <ajsthfldu@outlook.com>
2024-08-05 13:59:58 +0900

    fix: cors..

--- backend/src/main/java/ssafy/age/backend/config/WebMvcConfig.java
+++ backend/src/main/java/ssafy/age/backend/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package ssafy.age.backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,8 +10,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS")
                 .allowedOrigins("*")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name())
                 .allowedHeaders("*");
     }
 }
