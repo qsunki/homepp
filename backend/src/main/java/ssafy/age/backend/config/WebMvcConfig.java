50af17ab36b310b14ae7af34557ec2cec70c693a
qsunki <ajsthfldu@outlook.com>
2024-08-05 13:15:23 +0900

    fix: cors 허용안된 http 메소드 허용으로 변경

--- backend/src/main/java/ssafy/age/backend/config/WebMvcConfig.java
+++ backend/src/main/java/ssafy/age/backend/config/WebMvcConfig.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
+        registry.addMapping("/**").allowedMethods("/*").allowedOrigins("*");
     }
 }
