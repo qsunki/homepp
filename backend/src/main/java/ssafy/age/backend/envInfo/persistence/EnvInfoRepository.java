128f82964124eb15d57732e08a6de16c415fcec1
이효림 <hlleee@naver.com>
2024-08-06 12:32:18 +0900

    fix: 환경정보 최신하나만 가져오는 쿼리, optional ,예외 처리

--- backend/src/main/java/ssafy/age/backend/envInfo/persistence/EnvInfoRepository.java
+++ backend/src/main/java/ssafy/age/backend/envInfo/persistence/EnvInfoRepository.java
@@ -1,6 +1,7 @@
 package ssafy.age.backend.envInfo.persistence;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,4 +9,7 @@ public interface EnvInfoRepository extends JpaRepository<EnvInfo, Long> {
 
     @Query("SELECT e FROM EnvInfo e WHERE e.cam.id = :camId ORDER BY e.recordedAt ASC")
     List<EnvInfo> findAllByCamId(Long camId);
+
+    @Query("SELECT e FROM EnvInfo e WHERE e.cam.id = :camId ORDER BY e.id DESC LIMIT 1")
+    Optional<EnvInfo> findLatestByCamId(Long camId);
 }
--- backend/src/main/java/ssafy/age/backend/envInfo/service/EnvInfoService.java
+++ backend/src/main/java/ssafy/age/backend/envInfo/service/EnvInfoService.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ssafy.age.backend.cam.exception.CamNotFoundException;
 import ssafy.age.backend.cam.persistence.CamRepository;
+import ssafy.age.backend.envInfo.exception.EnvInfoNotFoundException;
 import ssafy.age.backend.envInfo.persistence.EnvInfo;
 import ssafy.age.backend.envInfo.persistence.EnvInfoRepository;
 import ssafy.age.backend.envInfo.web.EnvInfoReceivedDto;
@@ -35,9 +36,10 @@ public class EnvInfoService {
         if (!camRepository.existsById(camId)) {
             throw new CamNotFoundException();
         }
-        List<EnvInfo> envInfos = envInfoRepository.findAllByCamId(camId);
 
-        EnvInfo latestEnvInfo = envInfos.getLast();
-        return envInfoMapper.toEnvInfoResponseDto(latestEnvInfo);
+        return envInfoMapper.toEnvInfoResponseDto(
+                envInfoRepository
+                        .findLatestByCamId(camId)
+                        .orElseThrow(EnvInfoNotFoundException::new));
     }
 }
