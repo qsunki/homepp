803045c3483699281f8db74a05291a049fcd7a9e
qsunki <ajsthfldu@outlook.com>
2024-08-12 16:22:39 +0900

    fix: video 리스트 쿼리 수정

--- backend/src/main/java/ssafy/age/backend/video/persistence/VideoRepository.java
+++ backend/src/main/java/ssafy/age/backend/video/persistence/VideoRepository.java
@@ -10,14 +10,11 @@ import ssafy.age.backend.event.persistence.EventType;
 public interface VideoRepository extends JpaRepository<Video, Long> {
 
     @Query(
-            "SELECT v FROM Video v LEFT OUTER JOIN v.events e LEFT OUTER JOIN v.cam c JOIN c.member m WHERE "
-                    + ":email = m.email AND"
-                    + "(:types IS NULL OR e.type IN :types) AND "
-                    + "(:startDate IS NULL OR v.recordStartedAt >= :startDate) AND "
-                    + "(:endDate IS NULL OR v.recordStartedAt <= :endDate) AND "
-                    + "(:camId IS NULL OR c.id = :camId) AND "
-                    + "(:isThreat IS NULL OR v.isThreat = :isThreat)"
-                    + "ORDER BY v.id DESC")
+            "SELECT v FROM Video v LEFT JOIN v.events e LEFT JOIN v.cam c LEFT JOIN c.member m"
+                + " WHERE :email = m.email AND(:types IS NULL OR e.type IN :types) AND (:startDate"
+                + " IS NULL OR v.recordStartedAt >= :startDate) AND (:endDate IS NULL OR"
+                + " v.recordStartedAt <= :endDate) AND (:camId IS NULL OR c.id = :camId) AND"
+                + " (:isThreat IS NULL OR v.isThreat = :isThreat)ORDER BY v.id DESC")
     List<Video> findVideosByParams(
             @Param("email") String email,
             @Param("types") List<EventType> types,
