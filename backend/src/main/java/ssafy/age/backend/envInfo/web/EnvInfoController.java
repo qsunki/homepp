071accd9393238b5f5a10e5315d27045a8e57b18
유승현 <fview@naver.com>
2024-08-14 01:09:26 +0900

    feat: envInfo 관련 메서드 실행 전 인가 구현

--- backend/src/main/java/ssafy/age/backend/envInfo/web/EnvInfoController.java
+++ backend/src/main/java/ssafy/age/backend/envInfo/web/EnvInfoController.java
@@ -2,10 +2,12 @@ package ssafy.age.backend.envInfo.web;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import ssafy.age.backend.envInfo.service.EnvInfoService;
+import ssafy.age.backend.security.service.MemberInfoDto;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,12 +15,12 @@ public class EnvInfoController {
     private final EnvInfoService envInfoService;
 
     @GetMapping("/api/v1/cams/{camId}/envInfos")
-    public List<EnvInfoResponseDto> getEnvInfos(@PathVariable Long camId) {
-        return envInfoService.getEnvInfos(camId);
+    public List<EnvInfoResponseDto> getEnvInfos(@PathVariable Long camId, @AuthenticationPrincipal MemberInfoDto memberInfoDto) {
+        return envInfoService.getEnvInfos(camId, memberInfoDto.getMemberId());
     }
 
     @GetMapping("/api/v1/cams/{camId}/envInfo")
-    public EnvInfoResponseDto getEnvInfo(@PathVariable Long camId) {
-        return envInfoService.getEnvInfo(camId);
+    public EnvInfoResponseDto getEnvInfo(@PathVariable Long camId, @AuthenticationPrincipal MemberInfoDto memberInfoDto) {
+        return envInfoService.getEnvInfo(camId, memberInfoDto.getMemberId());
     }
 }
