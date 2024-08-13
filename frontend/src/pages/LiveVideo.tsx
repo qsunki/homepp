fbdf31861e1fa902a7dd46ef0a2964389f93ba55
dongwook <cabbi120101@gmail.com>
2024-08-13 09:55:59 +0900

    fix : videodetail live 레이아웃 조정

--- frontend/src/pages/LiveVideo.tsx
+++ frontend/src/pages/LiveVideo.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import DetailPlayer from '../components/videodetail/DetailPlayer';
 import DetailList from '../components/videodetail/DetailList';
 import { useVideoStore } from '../stores/useVideoStore';
+import styles from './VideoDetail.module.css'; // 모듈 CSS 파일을 import
 
 const LiveVideo: React.FC = () => {
   const { filteredVideos, selectedTypes, setSelectedTypes } = useVideoStore();
 
   return (
     <div className="w-full mx-auto px-4 my-20 flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4">
-      <div className="w-full lg:w-2/3">
+      <div className={styles.lgW2_3Important}>
         <DetailPlayer isLive={true} showDetails={false} />
       </div>
-      <div className="w-full lg:w-1/3">
+      <div className={styles.lgW1_3Important}>
         <DetailList
           showLiveThumbnail={false}
           videos={filteredVideos}
