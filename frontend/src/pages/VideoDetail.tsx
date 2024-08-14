d2a52ba306d5e788486cd80ddba65e088b2b7b4f
dongwook <cabbi120101@gmail.com>
2024-08-14 15:04:36 +0900

    fix: 동영상 디테일 진입 확인 수정

--- frontend/src/pages/VideoDetail.tsx
+++ frontend/src/pages/VideoDetail.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import DetailPlayer from '../components/videodetail/DetailPlayer';
 import DetailList from '../components/videodetail/DetailList';
 import { useVideoStore } from '../stores/useVideoStore';
-import styles from './VideoDetail.module.css'; // 모듈 CSS 파일을 import
+import styles from './VideoDetail.module.css';
 
 const VideoDetail: React.FC = () => {
-  const { filteredVideos, selectedTypes, setSelectedTypes } = useVideoStore();
+  const location = useLocation();
+  const {
+    filteredVideos,
+    selectedTypes,
+    setSelectedTypes,
+    selectedVideoId,
+    setSelectedVideoId,
+  } = useVideoStore();
+
+  // URL을 통해 전달된 selectedVideoId를 사용하여 초기 설정
+  useEffect(() => {
+    const { selectedVideoId: stateSelectedVideoId } = location.state || {};
+    if (stateSelectedVideoId && stateSelectedVideoId !== selectedVideoId) {
+      setSelectedVideoId(stateSelectedVideoId);
+    }
+  }, [location.state, selectedVideoId, setSelectedVideoId]);
 
   return (
     <div className="w-full mx-auto px-4 my-20 flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4">
