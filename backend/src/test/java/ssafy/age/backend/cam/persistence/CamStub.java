cd646a39e106989b04e9055ce3b98b0384df08a0
qsunki <ajsthfldu@outlook.com>
2024-12-28 18:30:56 +0900

    Refactor CamStub constructor formatting for readability

--- backend/src/test/java/ssafy/age/backend/cam/persistence/CamStub.java
+++ backend/src/test/java/ssafy/age/backend/cam/persistence/CamStub.java
@@ -4,7 +4,14 @@ import ssafy.age.backend.member.persistence.Member;
 
 public class CamStub extends Cam {
 
-    public CamStub(Long id, String name, String ip, String region, CamStatus status, Member member, String thumbnailUrl) {
+    public CamStub(
+            Long id,
+            String name,
+            String ip,
+            String region,
+            CamStatus status,
+            Member member,
+            String thumbnailUrl) {
         super(name, ip, region, status, member, thumbnailUrl);
         setId(id);
     }
