11ef1eaf5fa46949aa46c0137b29303d784febd1
qsunki <ajsthfldu@outlook.com>
2024-12-28 22:59:43 +0900

    Add test case for Cam creation in CamControllerTest

--- backend/src/test/java/ssafy/age/backend/cam/web/CamControllerTest.java
+++ backend/src/test/java/ssafy/age/backend/cam/web/CamControllerTest.java
@@ -1,8 +1,10 @@
 package ssafy.age.backend.cam.web;
 
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -13,14 +15,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import ssafy.age.backend.cam.service.CamService;
+import ssafy.age.backend.security.TestSecurityConfig;
 import ssafy.age.backend.security.service.MemberInfoDto;
 
 @WebMvcTest(CamController.class)
+@Import(TestSecurityConfig.class)
 class CamControllerTest {
 
     @Autowired MockMvc mockMvc;
@@ -60,6 +65,29 @@ class CamControllerTest {
         // when & then
         mockMvc.perform(get("/api/v1/cams").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isUnauthorized())
+                .andDo(print());
+    }
+
+    @DisplayName("이메일 정보를 받아 캠 생성을 할 수 있다.")
+    @Test
+    void createCam() throws Exception {
+        // given
+        String email = "test@example.com";
+        String clientIP = "0.0.0.0";
+        CamCreateRequestDto requestDto = new CamCreateRequestDto(email);
+        CamResponseDto responseDto = new CamResponseDto(1L, "living room", "living room url");
+        String request = objectMapper.writeValueAsString(requestDto);
+        String response = objectMapper.writeValueAsString(responseDto);
+        given(camService.createCam(anyString(), anyString())).willReturn(responseDto);
+
+        // when & then
+        mockMvc.perform(
+                        post("/api/v1/cams")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(request)
+                                .header("X-Forwarded-For", clientIP))
+                .andExpect(status().isOk())
+                .andExpect(content().json(response))
                 .andDo(print());
     }
 }
--- backend/src/test/java/ssafy/age/backend/security/TestSecurityConfig.java
+++ backend/src/test/java/ssafy/age/backend/security/TestSecurityConfig.java
@@ -24,7 +24,8 @@ public class TestSecurityConfig {
                 .authorizeHttpRequests(
                         authorize ->
                                 authorize
-                                        .requestMatchers(HttpMethod.POST, "/api/v1/members")
+                                        .requestMatchers(
+                                                HttpMethod.POST, "/api/v1/members", "/api/v1/cams")
                                         .permitAll()
                                         .anyRequest()
                                         .authenticated());
