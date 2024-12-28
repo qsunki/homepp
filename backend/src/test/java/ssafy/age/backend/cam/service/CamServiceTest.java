b6afcdd80c867e87b7f3cb5d244c494e667fb196
qsunki <ajsthfldu@outlook.com>
2024-12-28 23:57:08 +0900

    Add test for creating a camera with member email

--- backend/src/test/java/ssafy/age/backend/cam/service/CamServiceTest.java
+++ backend/src/test/java/ssafy/age/backend/cam/service/CamServiceTest.java
@@ -1,7 +1,7 @@
 package ssafy.age.backend.cam.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 
 import java.time.LocalDateTime;
@@ -18,6 +18,7 @@ import ssafy.age.backend.file.FileStorage;
 import ssafy.age.backend.member.persistence.Member;
 import ssafy.age.backend.member.persistence.MemberRepository;
 import ssafy.age.backend.member.persistence.MemberStub;
+import ssafy.age.backend.member.persistence.MemoryMemberRepository;
 import ssafy.age.backend.mqtt.MqttService;
 import ssafy.age.backend.notification.service.FCMService;
 import ssafy.age.backend.util.IPUtil;
@@ -32,6 +33,7 @@ class CamServiceTest {
     @Mock FileStorage fileStorage;
     @Mock IPUtil ipUtil;
     MemoryCamRepository fakeCamRepository = new MemoryCamRepository();
+    MemoryMemberRepository fakeMemberRepository = new MemoryMemberRepository();
 
     CamService camService;
 
@@ -100,5 +102,36 @@ class CamServiceTest {
         assertThat(camResponseDtos)
                 .extracting("name")
                 .containsExactlyInAnyOrder("living room", "kitchen", "bed room");
+    }
+
+    @DisplayName("이메일로 등록된 회원을 찾아 캠을 생성한다.")
+    @Test
+    void creatCam() {
+        // given
+        given(memberRepository.findByEmail(anyString()))
+                .willAnswer(
+                        invocation -> fakeMemberRepository.findByEmail(invocation.getArgument(0)));
+        given(camRepository.save(any(Cam.class)))
+                .willAnswer(invocation -> fakeCamRepository.save(invocation.getArgument(0)));
+        Long memberId = 1L;
+        String email = "test@example.com";
+        String clientIP = "0.0.0.0";
+        Member member =
+                new MemberStub(
+                        memberId,
+                        email,
+                        "testpassword",
+                        LocalDateTime.of(2024, 1, 1, 0, 0),
+                        "010-0000-0000");
+        fakeMemberRepository.save(member);
+
+        // when
+        CamResponseDto camResponseDto = camService.createCam(email, clientIP);
+
+        // then
+        Cam cam = fakeCamRepository.findCamsByMemberId(memberId).getFirst();
+        assertThat(camResponseDto.getCamId()).isEqualTo(cam.getId());
+        assertThat(camResponseDto.getName()).isEqualTo("Cam" + cam.getId());
+        assertThat(camResponseDto.getThumbnailUrl()).isBlank();
     }
 }
