90ca3c974845bcfcf7b469adcbe7f8d456e17603
qsunki <ajsthfldu@outlook.com>
2024-12-28 18:32:26 +0900

    Add test security configuration for Security setup in tests

---
+++ backend/src/test/java/ssafy/age/backend/security/TestSecurityConfig.java
@@ -0,0 +1,33 @@
+package ssafy.age.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class TestSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.formLogin(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        authorize ->
+                                authorize
+                                        .requestMatchers(HttpMethod.POST, "/api/v1/members")
+                                        .permitAll()
+                                        .anyRequest()
+                                        .authenticated());
+        return http.build();
+    }
+}
