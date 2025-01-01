5159f534db3718bb0051c720efdaf2fd64399f9e
qsunki <ajsthfldu@outlook.com>
2025-01-01 23:46:35 +0900

    Add constructor to RecordStatusDto

--- backend/src/main/java/ssafy/age/backend/envinfo/web/RecordStatusDto.java
+++ backend/src/main/java/ssafy/age/backend/envinfo/web/RecordStatusDto.java
@@ -5,6 +5,11 @@ import ssafy.age.backend.envinfo.persistence.RecordStatus;
 
 @Data
 public class RecordStatusDto {
-    private Long CamId;
+    private Long camId;
     private RecordStatus status;
+
+    public RecordStatusDto(Long camId, RecordStatus status) {
+        this.camId = camId;
+        this.status = status;
+    }
 }
