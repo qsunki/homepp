009569f62432069af70b144cc621ae83b2002d0d
qsunki <ajsthfldu@outlook.com>
2025-01-14 17:31:17 +0900

    Add MockitoBeans to BackendApplicationTests for MQTT

--- backend/src/test/java/ssafy/age/backend/BackendApplicationTests.java
+++ backend/src/test/java/ssafy/age/backend/BackendApplicationTests.java
@@ -2,9 +2,17 @@ package ssafy.age.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class BackendApplicationTests {
+
+    @MockitoBean MqttPahoClientFactory mqttClientFactory;
+    @MockitoBean MessageProducer inbound;
+    @MockitoBean MessageHandler mqttOutbound;
 
     @Test
     void contextLoads() {}
